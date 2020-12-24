58f8d10255d32f2dbcf65734fb11611183b5c969
Aviad Blumenfeld <aviadblu@gmail.com>
2020-12-24 20:02:53 +0200

    webpack starter

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import {Chief} from "./components/chief/chief"
+
+
+let a = new Chief();
+
+
---
+++ src/components/chief/chief.ts
@@ -0,0 +1,5 @@
+export  class Chief {
+    constructor() {
+        console.log("Hello Chief!!!")
+    }
+}
